3a6aa549e0a671ca6a796bb373fb69186c95230d
Lin <yunrou0921@stust.edu.tw>
2018-07-12 23:32:06 +0800

    first

---
+++ streamers.js
@@ -0,0 +1,122 @@
+const streamers = [
+    {
+        "userID": "0020c9f7-9f35-4d7f-8480-619e23faafb6",
+        "openID": "湘綾🐷🌺",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_6F65D96C-3935-4FB5-98F9-CE8E9482A737.jpg",
+        "score": 17744
+    },
+    {
+        "userID": "5c95515d-2617-4ae5-9732-5ecf3ed7a2e4",
+        "openID": "采婕御用執事🐷阿弟",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_E7573170-8AE3-4426-AA36-584B9B0373E3.jpg",
+        "score": 6447
+    },
+    {
+        "userID": "bb990c11-dfa8-4aec-af0a-eafe8afc94be",
+        "openID": "林彤🌝Grace",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_A3BDD043-3533-49E1-B29B-BA66E068BAE0.jpg",
+        "score": 1187
+    },
+    {
+        "userID": "3e9fce44-2c3b-4636-a05e-b50d410bb773",
+        "openID": "Beggie霸王貝姬",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_cfdf7594-c368-4d48-b15f-ef0c521c8aa2.jpg",
+        "score": 1176
+    },
+    {
+        "userID": "1522a1f8-6cdf-46fa-70a4-f5975c44addd",
+        "openID": "J咪💜",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_DE115918-B27B-44CC-8FB5-B83C2EF29FCD.jpg",
+        "score": 779
+    },
+    {
+        "userID": "2ba6aa60-86b4-4ada-b2c8-0dcdd7d6ce89",
+        "openID": "穎穎💋YiYi",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_AA1DB23C-AE26-4534-A290-AA98F08B4C5C.jpg",
+        "score": 521
+    },
+    {
+        "userID": "3d47a305-2baf-4b2e-9230-e737f2881d3c",
+        "openID": "卿卿親親",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_46C126AA-B7C3-40FB-AE44-98D1E4885B6B.jpg",
+        "score": 491
+    },
+    {
+        "userID": "37dc4c0c-a40d-4395-55bd-c6f1331139c4",
+        "openID": "幹嘛跟你說🙄洪綺",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_cc9292d7-bfa3-43e7-93d8-47a6d451ba1d.jpg",
+        "score": 408
+    },
+    {
+        "userID": "f0b9e9a6-6630-490d-92a7-09745bb5266d",
+        "openID": "毛孩教主🐶凡妮莎💕",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_A7D1CCF6-E885-402D-A785-87E85E5BAF76.jpg",
+        "score": 379
+    },
+    {
+        "userID": "d65d4ebf-b1d2-4599-740c-98907ca2204e",
+        "openID": "家家底家",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_FCB08EDA-C460-4D0C-B2C3-F6BC28294D0E.jpg",
+        "score": 373
+    },
+    {
+        "userID": "acb7ab15-3394-4924-87e0-54caf85a36f2",
+        "openID": "咪妹妹🍼Mia",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_6497A31C-F06E-4F66-B2E9-FEAB24A2EF56.jpg",
+        "score": 338
+    },
+    {
+        "userID": "51b7a1a7-39e8-49c9-4e7d-e454e2a376ed",
+        "openID": "夏琳charlene",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_95EA80F3-373A-4F24-B592-6AEA14BCA1B1.jpg",
+        "score": 296
+    },
+    {
+        "userID": "5f9faf37-6328-45b9-829a-7b031a9636a2",
+        "openID": "可愛柰柰🌸",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_B4584E9F-1958-4AB9-B13B-D0BF1CBDA6DF.jpg",
+        "score": 277
+    },
+    {
+        "userID": "6a16e5de-20d9-4758-4088-ee91848bcb83",
+        "openID": "kayla嘉嘉",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_1A09DAB4-A2E4-4F12-8A09-F966E8CCF208.jpg",
+        "score": 258
+    },
+    {
+        "userID": "cd981d47-6f39-43b7-6387-685ca48c9596",
+        "openID": "睫希gloria",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_BCD3E494-021A-488B-9F57-066A77540035.jpg",
+        "score": 242
+    },
+    {
+        "userID": "5224d356-db16-4c6c-90a4-f1beb4717e4e",
+        "openID": "上晏❤️rubis",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_efa7d78b-19d4-4cc5-a8d5-d5ca64490eb0.jpg",
+        "score": 238
+    },
+    {
+        "userID": "493a4577-1f6f-45b6-8f00-bbc9c19521ae",
+        "openID": "彈指神功琵琶俠女👌",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_09420729-D982-4A8B-822F-2C215B37F4F5.jpg",
+        "score": 228
+    },
+    {
+        "userID": "5f3ed15b-dcc2-479d-588a-5e331ce91fc2",
+        "openID": "甜茲茲🍭",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_3629e34b-b12e-47bf-8a02-481dd87490f5.jpg",
+        "score": 215
+    },
+    {
+        "userID": "84ef9540-63fa-40de-7f7a-5a384b83d592",
+        "openID": "羽婕兒",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_342793e8-3c11-47ee-86c4-3c75c9d5430e.jpg",
+        "score": 214
+    },
+    {
+        "userID": "38343410-408a-46fe-7b2d-68c06f6c5681",
+        "openID": "妮💕可以公主病😈",
+        "picture": "https://assets-17app.akamaized.net/THUMBNAIL_d7510a16-67f4-45bf-b4b1-85d150b00667.jpg",
+        "score": 210
+    },
+];
