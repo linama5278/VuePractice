97a0ac2e75960446e25b635097b82f02f11378b2
8549 <zochan0921@hotmail.com>
2018-12-21 16:31:43 +0800

    update 1221

---
+++ ddbs/btn-grp.js
@@ -0,0 +1,26 @@
+const btngrp = {
+  "zh-tw": {
+    "Calender": "行事曆",
+    "Contact": "電話",
+    "Map": "地址",
+    "Introduce": "介紹",
+    "Instagram": "IG",
+    "FQA": "問與答"
+  },
+  "en": {
+    "Calender": "Calender",
+    "Contact": "Contact",
+    "Map": "Map",
+    "Introduce": "Introduce",
+    "Instagram": "Instagram",
+    "FQA": "FQA"
+  },
+  "zh-mn": {
+    "Calender": "柚惜乾",
+    "Contact": "卡等喂",
+    "Map": "挖底家",
+    "Introduce": "無欸某",
+    "Instagram": "埃　基",
+    "FQA": "悶代誌"
+  }
+}
