90512d1b1113897fe1ac6f9ea634f70b56657e05
Lin <40746452+linama5278@users.noreply.github.com>
2019-04-30 14:15:10 +0800

    calendar

---
+++ ddbs/assets/languages/zh-tw.js
@@ -0,0 +1,20 @@
+// Day, Month
+var n_wordMonth = new Array("1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月");
+var n_wordDay_sun = "日";
+var n_wordDay_mon = "一";
+var n_wordDay_tue = "二";
+var n_wordDay_wed = "三";
+var n_wordDay_thu = "四";
+var n_wordDay_fri = "五";
+var n_wordDay_sat = "六";
+
+// View Button
+var n_calendar_view = "行事曆"
+var n_list_view = "列表"
+var n_back = "返回"
+
+// Calendar Button
+var n_prev_year = "上一年"
+var n_prev_month = "上個月"
+var n_next_month = "下個月"
+var n_next_year = "下一年"
